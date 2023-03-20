edb9c7ad66fbe72d37a43304551e894692682ab1
uditmar24 <uditmar24@gmail.com>
2023-03-21 02:22:01 +0530

    added jwt create and verify

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,9 @@
 const UserRepository=require('../repository/user-repository');
 
+const jwt=require('jsonwebtoken');
+
+const {JWT_KEY}=require('../config/serverConfig');
+
  class UserService{
     constructor(){
         this.userRepository = new UserRepository();
@@ -13,6 +17,26 @@ const UserRepository=require('../repository/user-repository');
             console.log("something went wrong in the service layer");
             throw error;
          }
+    }
+
+    createToken(user){
+        try {
+            const result=jwt.sign(user,JWT_KEY,{expiresIn:'1h'});
+            return result;
+        } catch (error) {
+            console.log("Soethig went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response=jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation",error);
+            throw error;
+        }
     }
  }
 
